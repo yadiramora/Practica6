7db8efec54e078c8568fc9aaf72d5c58df93ec74
yadiramora <almn.yadiracobos@cbtis66.edu.mx>
2022-10-12 19:52:48 -0500

    Practica6

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DadoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dado/dado.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent implements OnInit {
+  valor: number=1;
+  constructor() { }
+
+  ngOnInit() {
+    this.valor = Math.trunc(Math.random() * 6) + 1;
+  }
+
+}
